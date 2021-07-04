3903446de4cba7d75bf614cfa782ab0c0e92a452
Elveson Costa <elveson.esc@gmail.com>
2021-07-04 13:01:00 -0300

    Dia 2 NLW

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,11 @@
-import {useState} from "react";
+import { ButtonHTMLAttributes } from 'react'
 
-export function Button (){
-    const [counter, setCounter] = useState(0);
-    function increment(){
-        setCounter(counter +1)
-        console.log(counter);
-    }
-    return (
-        <button onClick={increment}>{counter}</button>
-    )
+import '../styles/button.scss';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function Button(props: ButtonProps) {
+  return (
+    <button className="button" {...props} />
+  )
 }
